de198af6f79d091f7efda7a4ca7ed960bd4a9f77
Pathao Ltd <pathaoltd@Pathaos-MacBook-Pro.local>
2021-09-06 00:54:59 +0600

    Coding challenge from git repo & string manipulation

---
+++ Day_2/CC2.js
@@ -0,0 +1,57 @@
+/* 
+1. Write a single line comment which says, comments can make code readable
+
+2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+6. Declare four variables without assigning values
+
+7. Declare four variables with assigned values
+
+8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console. 
+*/
+
+//Case 1
+//comments can make code readable
+
+//Case 2
+// Welcome to 30DaysOfJavaScript
+
+//Case 3
+/* comments ca make code readable,
+easy to reuse and informative */
+
+//Case 4
+let name = "Elon Musk", isEntrepreneur = true, estimatedNetWorth, initialWorth = null;
+
+//Case 5
+console.log(typeof name, typeof isEntrepreneur, typeof estimatedNetWorth, typeof initialWorth);
+
+//Case 6
+let var1,va2,var3,var4;
+
+//Case 7
+let day1 = "Saturday", day2 = "Sunday", day3 = "Monday", day4 = "Tuesday";
+
+//Case 8
+let firstName = "Sirivasa"; 
+let lastName = "Ramanujan"; 
+let maritalStatus = "Married";
+let country = "India";
+let age = 32;
+
+//Case 9
+// Same as case 4
+
+//Case 10
+let myAge = 23, yourAge = 32;
+console.log(myAge,yourAge); 
---
+++ Day_2/introduction.js
@@ -0,0 +1,18 @@
+//Template Literals(Template Strings)
+//Syntax
+let nam = "!!!";
+console.log(`String Literal Text ${nam}`);
+let a = 2, b = 3;
+console.log(`The sum of ${a} + ${b} = ${a+b}`);
+
+//String Manipulation
+let firstName = 'Elon';
+console.log(firstName.split());
+console.log(firstName.split(''));
+
+//String Replace
+let string  = "30 days of JavaScript";
+console.log(string.replace("30","14"));
+console.log(string.charAt(string.length-1));
+console.log(string.charCodeAt(3));
+console.log(string.indexOf('a'));
