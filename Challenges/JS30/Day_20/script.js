ff2d69888f88cd658304c5ba66f952de986a514a
Pathao Ltd <pathaoltd@Pathaos-MacBook-Pro.local>
2021-10-06 12:57:09 +0600

    Audio Input |Add audio as text(p) | Map & Join

---
+++ Challenges/JS30/Day_20/script.js
@@ -0,0 +1,30 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition  =new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result',(e)=>{
+    const transcript = Array.from(e.results)
+        .map((result)=> result[0])
+        .map((result) => result.transcript)
+        .join('');
+    console.log(transcript);
+    p.innerText = transcript;
+    if(e.results[0].isFinal)
+    {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+    if(transcript.includes('Unicorn'))
+    {
+        console.log('Getting Unicorn')
+    }
+
+})
+
+recognition.addEventListener('end',recognition.start);
+recognition.start();
